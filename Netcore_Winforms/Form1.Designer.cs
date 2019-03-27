f17b09919abe97863ad6957f4a6f4fbd0dc47321
Rainer Sigwald <raines@microsoft.com>
2019-03-27 14:41:28 -0500

    Add an icon, embedded into the resx

--- Netcore_Winforms/Form1.Designer.cs
+++ Netcore_Winforms/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.buttonExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -37,8 +38,9 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(800, 376);
+            this.label1.Size = new System.Drawing.Size(533, 241);
             this.label1.TabIndex = 1;
             this.label1.Text = "Hello .NET Core!";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -46,9 +48,10 @@
             // buttonExit
             // 
             this.buttonExit.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.buttonExit.Location = new System.Drawing.Point(0, 376);
+            this.buttonExit.Location = new System.Drawing.Point(0, 241);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(800, 74);
+            this.buttonExit.Size = new System.Drawing.Size(533, 47);
             this.buttonExit.TabIndex = 1;
             this.buttonExit.Text = "E&xit";
             this.buttonExit.UseVisualStyleBackColor = true;
@@ -56,11 +59,13 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 288);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonExit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
