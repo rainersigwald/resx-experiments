cb844db107690a485c399442450f4deee8b3564a
Rainer Sigwald <raines@microsoft.com>
2019-03-28 10:25:16 -0500

    resx2resx: read resources, convert to byte arrays

--- resx2resx/Program.cs
+++ resx2resx/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
 using System.Linq;
+using System.Resources;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +14,38 @@ namespace resx2resx
     {
         static void Main(string[] args)
         {
+            var resources = new List<(string name, byte[] bytes)>();
+
             // read resx
 
-            // serialize objects "like resourcewriter"
+            // TODO: support custom types by creating a resolver
+            ITypeResolutionService typeResolutionService = null;
+
+            using (ResXResourceReader reader = new ResXResourceReader(args[0]) { UseResXDataNodes = true })
+            {
+                var resEnum = reader.GetEnumerator();
+
+                while (resEnum.MoveNext())
+                {
+                    string name = (string)resEnum.Key;
+                    var node = (ResXDataNode)resEnum.Value;
+
+                    var type = node.GetValueTypeName(typeResolutionService);
+                    var value = node.GetValue(typeResolutionService);
+
+                    // serialize object "like resourcewriter"
+
+                    var bf = new BinaryFormatter();
+                    using (var ms = new MemoryStream())
+                    {
+                        bf.Serialize(ms, value);
+
+                        var bytes = ms.ToArray();
+
+                        resources.Add((name, bytes));
+                    }
+                }
+            }
 
             // write updated resx
         }
