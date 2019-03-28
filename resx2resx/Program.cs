6a265d329e1335573a544565269b005807aeb26c
Rainer Sigwald <raines@microsoft.com>
2019-03-28 15:45:22 -0500

    ToBase64WrappedString
    Impl stolen from https://github.com/dotnet/winforms/blob/4313acd6f0bcc7e711697ad471886599d35cf377/src/System.Windows.Forms/src/System/Resources/ResXDataNode.cs#L262-L283

--- resx2resx/Program.cs
+++ resx2resx/Program.cs
@@ -83,5 +83,32 @@ namespace resx2resx
             }
 
         }
+
+        private static string ToBase64WrappedString(byte[] data)
+        {
+            const int lineWrap = 80;
+            const string crlf = "\r\n";
+            const string prefix = "        ";
+            string raw = Convert.ToBase64String(data);
+            if (raw.Length > lineWrap)
+            {
+                StringBuilder output = new StringBuilder(raw.Length + (raw.Length / lineWrap) * 3); // word wrap on lineWrap chars, \r\n
+                int current = 0;
+                for (; current < raw.Length - lineWrap; current += lineWrap)
+                {
+                    output.Append(crlf);
+                    output.Append(prefix);
+                    output.Append(raw, current, lineWrap);
+                }
+                output.Append(crlf);
+                output.Append(prefix);
+                output.Append(raw, current, raw.Length - current);
+                output.Append(crlf);
+                return output.ToString();
+            }
+
+            return raw;
+        }
+
     }
 }
