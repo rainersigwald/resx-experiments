80f89d90a2f370d86186ce7dae24f409e7c6c0ec
Rainer Sigwald <raines@microsoft.com>
2019-03-28 09:54:07 -0500

    resx2resx from template

---
+++ resx2resx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resx2resx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read resx
+
+            // serialize objects "like resourcewriter"
+
+            // write updated resx
+        }
+    }
+}
