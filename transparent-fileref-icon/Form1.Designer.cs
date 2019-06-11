d455fe662f77fd6038e4620a4158b4dc06fc4fd2
Rainer Sigwald <raines@microsoft.com>
2019-06-11 11:12:53 -0500

    make background lime

--- transparent-fileref-icon/Form1.Designer.cs
+++ transparent-fileref-icon/Form1.Designer.cs
@@ -47,6 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
