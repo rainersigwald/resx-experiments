9e71ff6e7a1bca17e632000cea9b37f5d2f1d629
Olina Zhang <v-olzhan@microsoft.com>
2019-08-15 08:50:34 -0500

    PlayAudio_MemoryStreamyAudio_MemoryStream sample

---
+++ PlayAudio_MemoryStream/WindowsFormsApp12/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string file1 = @"c:\Ring08.wav";
+            List<byte> soundBytes = new List<byte>(File.ReadAllBytes(file1));
+            //create media player loading the first half of the sound file
+            MediaPlayer mPlayer = new MediaPlayer(soundBytes.ToArray());
+            //begin playing the file
+            mPlayer.Play();
+        }
+    }
+}
---
+++ PlayAudio_MemoryStream/WindowsFormsApp12/MediaPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp12
+{
+    class MediaPlayer
+    {
+        System.Media.SoundPlayer soundPlayer;
+        public MediaPlayer(byte[] buffer)
+        {
+            MemoryStream memoryStream = new MemoryStream(buffer, true);
+            soundPlayer = new System.Media.SoundPlayer(memoryStream);
+        }
+        public void Play() { soundPlayer.Play(); }
+        public void Play(byte[] buffer)
+        {
+            soundPlayer.Stream.Seek(0, SeekOrigin.Begin);
+            soundPlayer.Stream.Write(buffer, 0, buffer.Length);
+            soundPlayer.Play();
+        }
+    }
+}
